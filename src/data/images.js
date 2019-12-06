148029cbf2e6140c542cac1cf3cb9034ebccbdbb
AceWindstorm <47289009+AceWindstorm@users.noreply.github.com>
2019-12-05 23:57:23 -0500

    added additional content and changes

--- src/data/images.js
+++ src/data/images.js
@@ -1,6 +1,7 @@
 const images = [
     { id: 1, src: './img/back_muscles.jpeg', title: 'back_muscle', description: 'back_muscle image without labels' },
-    { id: 2, src: './img/leg_muscle.jpeg', title: 'leg_muscle', description: 'leg_muscle image without labels'}
+    { id: 2, src: './img/leg_muscle.jpeg', title: 'leg_muscle', description: 'leg_muscle image without labels'},
+    { id: 3, src: './img/arm_muscle.jpeg', title: 'arm_muscle', description: 'arm_muscle image without labels'}
   ];
   
 export default images;
