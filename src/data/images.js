f61d475f9e9f0370fa5aea6a16af6561bf8abaf7
AceWindstorm <47289009+AceWindstorm@users.noreply.github.com>
2019-12-05 23:01:45 -0500

    Add files via upload
    randomization working, added a card. will add more.

--- src/data/images.js
+++ src/data/images.js
@@ -1,5 +1,6 @@
 const images = [
-    { id: 1, src: './img/back_muscles.jpeg', title: 'back_muscle', description: 'back_muscle image without labels' }
+    { id: 1, src: './img/back_muscles.jpeg', title: 'back_muscle', description: 'back_muscle image without labels' },
+    { id: 2, src: './img/leg_muscle.jpeg', title: 'leg_muscle', description: 'leg_muscle image without labels'}
   ];
   
 export default images;
